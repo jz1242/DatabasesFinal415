b70d2cd90551afaec0a9f3047f10f43bae9ff0c8
Jason <jz1242@gmail.com>
2017-12-17 00:11:18 -0500

    Testing out flask, added config parsing, did basic querying

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flaskext.mysql import MySQL
+from flask import jsonify
+import configparser
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = config.get('database', 'db_user')
+app.config['MYSQL_DATABASE_PASSWORD'] = config.get('database', 'db_password')
+app.config['MYSQL_DATABASE_DB'] = config.get('database', 'db_dbname')
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql = MySQL()
+mysql.init_app(app)
+cur = mysql.connect().cursor()
+
+
+#test query connecting to old database and displaying the query
 @app.route("/")
 def main():
-    return render_template('index.html')
+    cur.execute("SELECT FName FROM Rawscores ")
+    return jsonify(data=cur.fetchall())
+    #return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
