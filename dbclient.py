f3d842efba1752772b8d20cfd3aae4618e1d0263
Archan Patel <archan.patel890@gmail.com>
2017-12-21 23:47:19 -0500

    Added template for dbclient methods.

--- dbclient.py
+++ dbclient.py
@@ -6,7 +6,25 @@ config.read('config.ini')
 
 cnx = mysql.connector.connect(user=config.get('database', 'db_user'),
                               password=config.get('database', 'db_password'),
-                              host='127.0.0.1',
-                              database=config.get('database', 'db_dbname'))
+                              database=config.get('database', 'db_dbname'),
+                              host='127.0.0.1')
 
-def 
+
+def get_food_groups():
+    cursor = cnx.cursor()
+    cursor.execute('Select FdGrp_Desc From FOOD_GROUP_DESCRIPTION')
+    column_names = cursor.column_names
+    rows = cursor.fetchall()
+    cursor.close()
+
+    return column_names, rows
+
+
+def get_all_foods():
+    cursor = cnx.cursor()
+    cursor.execute('Select Long_Desc, Shrt_Desc From FOOD_DESCRIPTION')
+    column_names = cursor.column_names
+    rows = cursor.fetchall()
+    cursor.close()
+
+    return column_names, rows
